ee330e87a318219496f5c297f08d8213f168e884
khalifa-pendrops <dikee5200@gmail.com>
2025-07-14 05:44:43 +0100

    installed lodash types and commented out dark mode for now

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import OurRoute from "./routes";
 import "./App.css";
 
 function App() {
-  const toggleDarkMode = () => {
-    document.documentElement.classList.toggle("dark");
-  };
+  // const toggleDarkMode = () => {
+  //   document.documentElement.classList.toggle("dark");
+  // };
   return (
     <>
       {/* <h2 className="text-4xl font-semibold">Hafa, make we start here!</h2> */}
